6d27630091159ab3b03228d84bccebdd772e43de
zabellfour <zabell2814@gmail.com>
2017-02-05 22:39:24 +0200

    added fuull screen banner

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,3 +1,5 @@
 module.exports = [
-  './bower/jquery/dist/jquery.js'
+  './bower/jquery/dist/jquery.js',
+  './bower/smooth-scroll/dist/js/smooth-scroll.js',
+  './bower/vide/dist/jquery.vide.js'  
 ];
