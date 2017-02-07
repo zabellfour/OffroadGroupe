05e0edcd5869c9110143041ceb12623f6b2e71f3
zabellfour <zabell2814@gmail.com>
2017-02-07 23:46:07 +0200

    added gallery section

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,5 +1,7 @@
 module.exports = [
   './bower/jquery/dist/jquery.js',
   './bower/smooth-scroll/dist/js/smooth-scroll.js',
-  './bower/vide/dist/jquery.vide.js'  
+  './bower/vide/dist/jquery.vide.js',
+  './bower/fancybox/source/jquery.fancybox.js',
+  './bower/video.js/dist/video.js'    
 ];
