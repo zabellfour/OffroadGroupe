de0acf26a2c6e395082b75c18530120d16cd6956
zabellfour <zabell2814@gmail.com>
2017-02-14 23:33:31 +0200

    added video gallery page

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@
     $(function() {
         smoothScroll.init({
             selector: '.smooth-scroll',
-            speed: 2500,
+            speed: 1000,
             easing: 'easeInOutCubic',
             offset: 0
         });
@@ -23,90 +23,62 @@ $(document).ready(function() {
     });
 });
 
+
 $(document).ready(function() {
-    $(".various").fancybox({
+    $.fancybox({
         maxWidth: 1000,
         maxHeight: 800,
         fitToView: false,
-        width: '70%',
-        height: '70%',
+        width: '100%',
+        height: '100%',
         autoSize: false,
         closeClick: true,
         openEffect: 'none',
         closeEffect: 'none'
     });
+    $.stellar({
+        positionProperty: 'transform',
+        hideDistantElements: false,
+        horizontalScrolling: false
+
+    });
+
+    jQuery(function() {
+        initCustomMap();
+    });
+
+    function setMapEvents() {
+        jQuery('#map').css("pointer-events", "none");
+        jQuery('.map-holder').on("click", function() {
+            jQuery('#map').css("pointer-events", "auto");
+        });
+
+
+        jQuery('#map').on("mouseout", function() {
+            jQuery(this).css("pointer-events", "none");
+        });
+    }
+
+    function initCustomMap() {
+        var myLatLng = { lat: 49.947804, lng: 36.397481 };
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 17,
+            center: myLatLng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var image = {
+            url: 'build/images/logo-map-marker.png'
+        };
+
+        var marker = new google.maps.Marker({
+            position: { lat: 49.948401, lng: 36.396368 },
+            map: map,
+            icon: image
+        });
+
+        setMapEvents()
+
+    }
 });
-
-$.fn.moveIt = function(){
-  var $window = $(window);
-  var instances = [];
-  
-  $(this).each(function(){
-    instances.push(new moveItItem($(this)));
-  });
-  
-  window.onscroll = function(){
-    var scrollTop = $window.scrollTop();
-    instances.forEach(function(inst){
-      inst.update(scrollTop);
-    });
-  }
-}
-
-var moveItItem = function(el){
-  this.el = $(el);
-  this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
-
-moveItItem.prototype.update = function(scrollTop){
-  var pos = scrollTop / this.speed/4;
-  this.el.css('transform', 'translateY(' + -pos + 'px)');
-};
-
-// Initialization
-$(function(){
-  $('[data-scroll-speed]').moveIt();
-});
-
-jQuery(function() {
-    initCustomMap();
-});
-
-
-
-function initCustomMap() {
-    var myLatLng = { lat: 49.947804, lng: 36.397481 };
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: myLatLng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var image = {
-        url: 'build/images/logo-map-marker.png'
-    };
-
-    var marker = new google.maps.Marker({
-        position: { lat: 49.948401, lng: 36.396368 },
-        map: map,
-        icon: image
-    });
-
-    setMapEvents()
-
-}
-
-function setMapEvents() {
-    jQuery('#map').css("pointer-events", "none");
-    jQuery('.map-holder').on("click", function() {
-        jQuery('#map').css("pointer-events", "auto");
-    });
-
-
-    jQuery('#map').on("mouseout", function() {
-        jQuery(this).css("pointer-events", "none");
-    });
-}
-
-
--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,8 +1,10 @@
 module.exports = [
   './bower/jquery/dist/jquery.js',
+  './bower/jquery-migrate/jquery-migrate.js',
   './bower/smooth-scroll/dist/js/smooth-scroll.js',
   './bower/vide/dist/jquery.vide.js',
   './bower/fancybox/source/jquery.fancybox.js',
   './bower/video.js/dist/video.js',
-  './bower/jquery.stellar/jquery.stellar.js'    
+  './bower/jquery.stellar/src/jquery.stellar.js',
+
 ];
